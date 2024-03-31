c4a9c21a5e1caf37750e0e9220247bb77ea438e8
dragolea <dragolea@yahoo.com>
2024-03-31 20:38:45 +0300

    Small changes to middlewares due to eslint

--- srv/middleware/MiddlewareEntity1.ts
+++ srv/middleware/MiddlewareEntity1.ts
@@ -1,6 +1,6 @@
-import { type TypedRequest } from '@sap/cds';
-import { type MiddlewareImpl, type Next } from '@dxfrontier/cds-ts-dispatcher';
-import { type Book } from '#cds-models/CatalogService';
+import type { TypedRequest } from '@sap/cds';
+import type { MiddlewareImpl, Next } from '@dxfrontier/cds-ts-dispatcher';
+import type { Book } from '#cds-models/CatalogService';
 
 export class MiddlewareEntity1 implements MiddlewareImpl {
   public async use(req: TypedRequest<Book>, next: Next) {
